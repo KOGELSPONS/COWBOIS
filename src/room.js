4e115a63f1ad22f055b4f5ca34a6ce356583b0db
Merijn <38210310+KOGELSPONS@users.noreply.github.com>
2023-02-01 15:35:29 +0000

    Door Placements: - We fixed that you can have different locations for the doors

--- src/room.js
+++ src/room.js
@@ -4,25 +4,25 @@ let map = [
     1, 1, 0,
     0, 1, 1
 ];
-let left = [
+let doorleft = [
     0, 1, 1,
     0, 0, 0,
     0, 1, 0,
     0, 0, 1
 ];
-let right = [
+let doorright = [
     1, 1, 0,
     0, 0, 0,
     1, 0, 0,
     0, 1, 0
 ];
-let top = [
+let doortop = [
     0, 0, 0,
     0, 1, 0,
     0, 1, 0,
     0, 1, 0
 ];
-let bottom = [
+let doorbottom = [
     0, 1, 0,
     0, 1, 0,
     0, 1, 0,
@@ -32,27 +32,31 @@ let bottom = [
 let rooms = [];
 
 class Room{
-  constructor(x,y,w,h,type){
+  constructor(x,y,w,h,l,r,t,b){
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
+    this.left =  l
+    this.right = r
+    this.top =  t
+    this.bottom = b
   }
   show(){
     fill(200);
     rect(this.x, this.y, this.w, this.h); //room
-    if (this.type == 1){
+    if (this.left == 1){
       fill(255);
       rect(this.x, this.y+this.h/2-50, 15, 100);//door (links)
-    } else if (this.type == 2){
+    } if (this.right == 1){
       fill(255);
       rect(this.x + this.w - 15, this.y+this.h/2-50, 15, 100);//door (rechts)
-    } else if (this.type == 3){
+    } if (this.top == 1){
       fill(255);
       rect(this.x + this.w/2 - 50, this.y, 100, 15);//door (boven)
-    } else if (this.type == 4){
+    } if (this.bottom == 1){
       fill(255);
-      rect(this.x + this.w - 15, this.y+this.h/2-50, 15, 100);//door (onder)
+      rect(this.x + this.w/2 - 50, this.y + this.h-15, 100, 15);//door (onder)
     }
   }
 }
@@ -60,14 +64,18 @@ class Room{
 function drawTiles(map, d_cols, roomsize_x, roomsize_y) {
   for (let i = map.length - 1; i > -1; --i) {
     let value = map[i];
+    let l = doorleft[i];
+    let r = doorright[i];
+    let t = doortop[i];
+    let b = doorbottom[i];
     
     // distenation x , y
     let dx = (i % d_cols) * roomsize_x;
     let dy = Math.floor(i / d_cols) * roomsize_y;
     
     // render image
-    if(value >= 1){
-      rooms.push(new Room(dx, dy, roomsize_x, roomsize_y, value));
+    if(value == 1){
+      rooms.push(new Room(dx, dy, roomsize_x, roomsize_y, l,r,t,b));
     }
   }
 }
